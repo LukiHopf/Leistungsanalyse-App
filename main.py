fd96f3afeda6ae2f5d0a9800d7079ddb7bf610ac
LukiHopf <163841124+LukiHopf@users.noreply.github.com>
2024-04-12 16:38:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,29 @@ if __name__ == "__main__":
     # Build a Supervisor
     #supervisor1 = build_person("Lukas", "Höpflinger", "male", 21)
     print("Bitte geben Sie die Daten des Supervisors ein:")
-    supervisor1 = build_person(input("Vorname:" ), input("Nachname:" ),  input("Geschlecht (englisch):"), int(input("Alter:")))
+    supervisor1 = build_person(
+        input("Vorname:" ), 
+        input("Nachname:" ), 
+        input("Geschlecht (englisch):"), 
+        int(input("Alter:")))
     
     #Build a Subject
     #subject1 = build_person("Max", "Mustermann", "male", 25)
     print("Bitte geben Sie die Daten des Subjects ein:")
-    subject1 = build_person(input("Vorname:" ), input("Nachname:" ),  input("Geschlecht (male or female:"), int(input("Alter:")))
+    subject1 = build_person(
+        input("Vorname:" ), 
+        input("Nachname:" ), 
+        input("Geschlecht (male or female:"), 
+        int(input("Alter:")))
 
     # Build an experiment
     #experiment1 = build_experiment("First Experiment", "2024-04-05", supervisor1, subject1)
     print("Bitte geben Sie die Daten des Experiments ein:")
-    experiment1 = build_experiment(input("Experimentname:" ), input("Datum:" ), supervisor1, subject1)
+    experiment1 = build_experiment(
+        input("Experimentname:" ), 
+        input("Datum:" ), 
+        supervisor1, 
+        subject1)
 
     #Print Experiment
     print(experiment1)
